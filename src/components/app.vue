5239b340f0018b14077e2d1c994f5808736168c4
zxfjd3g <258147149@qq.com>
2017-09-08 16:49:37 +0800

    update

---
+++ src/components/app.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <search :setSearchName="setSearchName"></search>
+    <xxx-main :searchName="searchName"></xxx-main>
+  </div>
+</template>
+
+<script>
+  import search from './search.vue'
+  import main from './main.vue'
+  export default {
+
+    data () {
+      return {
+        searchName: ''
+      }
+    },
+
+    methods: {
+      setSearchName (searchName) {
+        this.searchName = searchName
+      }
+    },
+
+    components: {
+      search,
+      'xxx-main': main
+    }
+  }
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
-import app from './app.vue'
+import app from './components/app.vue'
 
 // 声明使用插件
 Vue.use(VueResource) // 内部调用VueResource.install()给vm/组件对象添加属性$http
